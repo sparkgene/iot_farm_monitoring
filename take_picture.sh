10aa32971f61a81dc19b588ad02c4c2b3d1de56d
Jun Ichikawa <jichikawa@r.recruit.co.jp>
2016-04-17 15:16:25 +0900

    fix dir

--- take_picture.sh
+++ take_picture.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-temp_dir="/opt/farm_data/photo/"
+photo_dir="/opt/pi_farm/data/photo"
 file_name=$(date +"%Y-%m-%d-%H%M%S").jpg
 
-mkdir -p $temp_dir
-
-raspistill -o $temp_dir$file_name
+raspistill -o ${photo_dir}${file_name}
