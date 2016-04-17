07458102c631dfd5ab960fd5ac576c7527ff0c83
Jun Ichikawa <jun1ka0@gmail.com>
2016-04-17 22:53:56 +0900

    change photo size

--- take_picture.sh
+++ take_picture.sh
@@ -3,4 +3,4 @@
 photo_dir="/opt/pi_farm/data/photos"
 file_name=$(date +"%Y-%m-%d-%H%M%S").jpg
 
-raspistill -w 720 -o ${photo_dir}/${file_name}
+raspistill -w 1280 -h 720 -o ${photo_dir}/${file_name}
